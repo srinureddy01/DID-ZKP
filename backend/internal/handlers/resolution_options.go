941d2e20233c3f8c2be7b344889074c4bda5bac5
GUDURU SRINIVASA REDDY <g.svreddyy@gmail.com>
2026-06-02 15:03:31 -0500

    Add ResolutionOptions struct to handlers package

---
+++ backend/internal/handlers/resolution_options.go
@@ -0,0 +1,9 @@
+// backend/internal/handlers/resolution_options.go
+package handlers
+
+type ResolutionOptions struct {
+	Accept      string
+	VersionId   string
+	VersionTime string
+	NoCache     bool
+}
